d9eb402d5d778768ea1fee80ecb77280daa57a61
daeyongJung1 <jung.829@osu.edu>
2024-07-18 03:08:37 -0400

    Docker updated

--- elasticsearch-frontend/src/Pages/Home.jsx
+++ elasticsearch-frontend/src/Pages/Home.jsx
@@ -52,6 +52,7 @@ export default function Home() {
 
     return (
         <div className="flex grow flex-col items-center justify-center bg-gray-100">
+            <div className="title">Recipe Allergy Scanner</div>
             <textarea
                 type={inputType === 'link' ? "url" : "text"}
                 value={userInput}
